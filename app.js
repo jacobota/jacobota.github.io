35b805f7f8164391f4cebd137250c665f25eb504
jacobota <otajacob@yahoo.com>
2022-07-07 13:40:29 -0700

    Added coloring and a couple animations

--- app.js
+++ app.js
@@ -4,15 +4,4 @@
  * @author Jacob Ota
  */
 
-let dropdown_button = document.querySelector(".fa-circle-chevron-down");
-let buttonActive = false;
 
-//add event listener on a click
-dropdown_button.addEventListener("click", () => {
-    if(buttonActive) {
-        dropdown_button.className = "fa-solid fa-circle-chevron-down"
-    } else {
-        dropdown_button.className = "fa-solid fa-circle-chevron-up"
-    }
-    buttonActive = !buttonActive;
-})
